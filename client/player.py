b1fac321df3ad56e91e407dcd5198cfbe62f3826
Danya Generalov <danya02@me.com>
2018-01-03 10:15:23 +0300

    Adding call to superclass init.

--- client/player.py
+++ client/player.py
@@ -4,5 +4,6 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
+        super().__init__()
         self.image = pygame.Surface((1,1))
         self.rect = self.image.get_rect()
