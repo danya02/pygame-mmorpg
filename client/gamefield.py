b21c84b5c98b42358ee4f957a697126546966396
Danya Generalov <danya02@me.com>
2018-01-03 10:13:28 +0300

    Adding basic drawing functions.

--- client/gamefield.py
+++ client/gamefield.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+import pygame
 
 
 class GameField:
-    pass
+    def __init__(self):
+        self.surface = pygame.Surface((1, 1))
+        self.players = pygame.sprite.Group()
+
+    def update(self, data):
+        pass
+
+    def draw(self, surface):
+        surface.blit(self.surface, (0, 0))
+        self.players.draw(surface)
--- client/player.py
+++ client/player.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+import pygame
 
 
-class Player:
-    pass
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = pygame.Surface((1,1))
+        self.rect = self.image.get_rect()
