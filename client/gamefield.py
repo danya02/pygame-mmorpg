e4309f374f3ac56ad1af93202bc8336718e6a520
Danya Generalov <danya02@me.com>
2018-01-04 10:10:23 +0300

    Separating players, entities and NPCs and adding a send function.

--- client/gamefield.py
+++ client/gamefield.py
@@ -6,16 +6,27 @@ class GameField:
     def __init__(self):
         self.surface = pygame.Surface((1, 1))
         self.players = pygame.sprite.Group()
+        self.entities = pygame.sprite.Group()
+        self.npcs = pygame.sprite.Group()
+        self.send_message = lambda x: None
 
     def load(self, data):
         pass  # TODO: create players, bg, &c.
 
-
     def update(self, data):
-        self.players.update(data)
-        if len(data) != 0:
+        self.players.update(data['players'])
+        self.entities.update(data['entities'])
+        self.npcs.update(data['npcs'])
+        if len(data['players']) != 0:
+            pass  # TODO: create classes.
+        if len(data['entities']) != 0:
+            pass  # TODO: create classes.
+        if len(data['npcs']) != 0:
             pass  # TODO: create classes.
 
     def draw(self, surface):
         surface.blit(self.surface, (0, 0))
         self.players.draw(surface)
+
+    def send_from_player(self, data):
+        self.send_message(data)
