af9ea7f8ffcc010fadea5722f430df02d84ff9d8
Danya Generalov <danya02@me.com>
2018-01-06 12:13:53 +0300

    Adding fire effect

--- client/effects.py
+++ client/effects.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import pygame
 import threading
+import random
 
 
 class Effect(threading.Thread):
@@ -25,3 +26,16 @@ class Poison(Effect):
                     color = self.target.image.get_at((i, j))
                     color.g = min(color.g + 64, 255)
                     self.target.image.set_at((i, j), color)
+
+
+class Fire(Effect):
+    def __init__(self, target):
+        super().__init__(target)
+
+    def run(self):
+        if self.target.image.get_at((0, 0)) != pygame.Color(255, 0, 0, 255):
+            self.target.image.set_at((0, 0), pygame.Color(255, 0, 0, 255))
+            for i in range(self.target.image.get_width()):
+                for j in range(self.target.image.get_height()):
+                    if random.randint(0, 1000)>750:
+                        self.target.image.set_at((i, j), pygame.Color('red'))
